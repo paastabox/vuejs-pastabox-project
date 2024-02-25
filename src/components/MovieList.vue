2accb0f00c4648986d8bdcc3d98930e7defede23
paastabox <celian1905@outlook.fr>
2024-02-25 14:11:49 +0100

    API update

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
         <!-- list of movies -->
+        <v-card variant="outlined" v-for="movie in movies">
+            <v-card-title>
+                {{ movie.title }}
+            </v-card-title>
+            <v-img :src="config.url.image + movie.poster_path" />
+        </v-card>
     </div>
 </template>
+
 <script>
+import config from '@/config.json';
 
 export default {
     name: 'MoviesList',
+    data() {
+        return {
+            movies: []
+        }
+    },
+    beforeMount() {
+        console.log('beforeMount');
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer ' + config.api_key
+            }
+        };
 
+        fetch(config.url.movie_list, options)
+            .then(response => response.json())
+            .then(data => {
+                this.movies = data.results;
+            })
+            .catch(error => console.error(error));     
+    }
 }
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <template>
-
+    <div>
+        <MovieList />
+    </div>
 </template>
 
 <script>
+import MovieDetail from '@/components/MovieDetail.vue';
+import MovieList from '@/components/MovieList.vue';
+import MoviePagination from '@/components/MoviePagination.vue';
+import movieSearch from '@/components/movieSearch.vue';
 
 export default {
-  name: 'HomeView',
+    name: 'HomeView',
+    components: {
+        MovieDetail,
+        MovieList,
+        MoviePagination,
+        movieSearch
+    }
 }
 
 </script>
